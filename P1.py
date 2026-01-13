801c0d2338aceabf1b18d705bed3cf6bd4ecd65d
Anasop16 <SBMPC.Student@SVKMGRP.COM>
2026-01-13 13:43:57 +0530

    Jan13

--- P1.py
+++ P1.py
@@ -1,6 +1,33 @@
-def greet():
-    print("Hello World")
+def getnum():
+    num = int(input("Enter a number"))
+    return num
+
+def pr(num):
+    if check(num)%2:
+        print(f"{num} is Odd")
+    else:
+        print("{} is Even".format(num))
+    return 
+
+def no(num):
+    sum=0
+    while num>0:
+        sum+=1
+        num = num //10
+    return sum
+
+
+def fact(num):
+    for i in range(1,num):
+        num*=i
+    return num
+
+def check(num):
+    return bool(num % 2)
+
 def main():
-    greet()
+    num = getnum()
+    print(no(num))
+
 if __name__=="__main__":
     main()
